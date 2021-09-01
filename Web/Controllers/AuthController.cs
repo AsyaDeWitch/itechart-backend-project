a97c5cef890141baed3fa8ac95b6d10e5b67b99b
AsyaDeWitch <denisevich.asya@gmail.com>
2021-09-01 14:46:07 +0300

    added AuthController

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("sign-in")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("sign-up")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("email-confirmation")]
+        public IActionResult EmailConfirm()
+        {
+            return View();
+        }
+    }
+}
